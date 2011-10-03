ee01347e1a87e88656ca23e04cc6067e28a66269
vanevery <vanevery@walking-productions.com>
2011-10-03 11:35:46 -0400

    Better follow me code

--- src/com/mobvcasting/pageasplace/PageAsPlace.java
+++ src/com/mobvcasting/pageasplace/PageAsPlace.java
@@ -3,6 +3,7 @@ package com.mobvcasting.pageasplace;
 import com.wowza.wms.application.*;
 import com.wowza.wms.amf.*;
 import com.wowza.wms.client.*;
+import com.wowza.wms.logging.WMSLogger;
 import com.wowza.wms.module.*;
 import com.wowza.wms.request.*;
 import com.wowza.wms.stream.*;
@@ -10,6 +11,8 @@ import com.wowza.wms.rtp.model.*;
 import com.wowza.wms.httpstreamer.model.*;
 import com.wowza.wms.httpstreamer.cupertinostreaming.httpstreamer.*;
 import com.wowza.wms.httpstreamer.smoothstreaming.httpstreamer.*;
+import java.net.*;
+import java.io.*;
 
 public class PageAsPlace extends ModuleBase {
 	
@@ -62,30 +65,65 @@ public class PageAsPlace extends ModuleBase {
 	
 	public void newUrl(IClient client, RequestFunction function, AMFDataList params) {
 		getLogger().info("newUrl");
+		getLogger().info("AMFDataList: " + params.toString());
+		
+		String passedUrl = getParamString(params,PARAM1);
+		getLogger().info("newUrl: " + passedUrl);
+		
+		// Make HTTP Request here!
+		/*
+		URL theUrl = new URL("http://www.yahoo.com/");
+		Ê Ê Ê Ê URLConnection yc = yahoo.openConnection();
+		Ê Ê Ê Ê BufferedReader in = new BufferedReader(
+		Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê new InputStreamReader(
+		Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê yc.getInputStream()));
+		Ê Ê Ê Ê String inputLine;
+
+		Ê Ê Ê Ê while ((inputLine = in.readLine()) != null) 
+		Ê Ê Ê Ê Ê Ê System.out.println(inputLine);
+		Ê Ê Ê Ê in.close();
+		Ê Ê }
+		}
+		*/
 		
 		String previousUrl = "";
 		for (int i = 0; i < connectedClients.length; i++) {
 			if (connectedClients[i] != null && connectedClients[i].client.equals(client)) {
 				previousUrl = connectedClients[i].currentURL;
-				//TODO
-				// Need to set currentURL
-				// How to know what the URL is?  In params?
-				connectedClients[i].currentURL = "";
+				// Set currentURL
+				connectedClients[i].currentURL = passedUrl;
 			}
 		}
+		getLogger().info("Previous URL: " + previousUrl);
 		
 		for (int j = 0; j < connectedClients.length; j++) {
-			if (connectedClients[j] != null && !connectedClients[j].client.equals(client) && connectedClients[j].currentURL.equals(previousUrl)) {
+			//if (connectedClients[j] != null && !connectedClients[j].client.equals(client) && connectedClients[j].currentURL.equals(previousUrl)) {
+			//if (connectedClients[j] != null && connectedClients[j].currentURL.equals(previousUrl)) {
+			if (connectedClients[j] != null) {
+				getLogger().info("Have a client with the right previous URL");
 				//TODO
 				// Need to set currentURL
-				connectedClients[j].currentURL = "";
-				//TODO
-				// Need to tell client to navigate
-				// How to call a function on the client
+				connectedClients[j].currentURL = passedUrl;
+
+				// Tell client to navigate
+				// This is a dummy class to get anything back from the client
+				class s2cParamsSimpleResult extends ModuleBase implements IModuleCallResult
+				{
+					public void onResult(IClient client, RequestFunction function, AMFDataList params)
+					{
+						WMSLogger log = getLogger();
+						String param1 = getParamString(params, PARAM1);
+						log.info(param1);
+					}
+				}
+				// Call the method on the client
+				connectedClients[j].client.call("newUrl", new s2cParamsSimpleResult(), passedUrl);
+
+			}
+			else if (connectedClients[j] != null) {
+				getLogger().info("Wrong previous URL");
 			}
 		}
-		
-		
 	}
 	
 	public void onAppStart(IApplicationInstance appInstance) {
@@ -104,14 +142,20 @@ public class PageAsPlace extends ModuleBase {
 			AMFDataList params) {
 		getLogger().info("onConnect: " + client.getClientId());
 		
+		String passedUrl = getParamString(params,PARAM1);
+		getLogger().info("onConnect URL: " + passedUrl);
+
 		client.setStreamVideoSampleAccess(IClient.VIDEOSAMPLE_ACCESS_ALL);
 
 		boolean done = false;
 		for (int i = 0; i < connectedClients.length && !done; i++) {
 			if (connectedClients[i] == null) {
 				connectedClients[i] = new PAPClient(client);
+				connectedClients[i].currentURL = passedUrl;
 				client.acceptConnection();
 				done = true;
+				
+				getLogger().info("New Client Accepted");
 			}
 		}
 		
